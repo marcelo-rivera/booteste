2d91ee182afee8162d04a5c9c4e28acbea547604
marcelo-rivera <marceloriveras@gmail.com>
2024-10-10 09:50:06 -0300

    componente perfil

--- src/app/components/user/perfil/perfil.component.ts
+++ src/app/components/user/perfil/perfil.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AbstractControlOptions, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ValidatorField } from '@app/helpers/ValidatorField';
 
 @Component({
   selector: 'app-perfil',
@@ -7,9 +9,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PerfilComponent implements OnInit {
 
-  constructor() { }
+  form!: FormGroup;
 
-  ngOnInit() {
+  get f(): any {
+    return this.form.controls;
   }
 
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.validation();
+  }
+
+  public validation(): void {
+
+    const formOptions: AbstractControlOptions = {
+      validators: ValidatorField.MustMatch('senha','senhaConfirm')
+    };
+
+    this.form = this.fb.group({
+    titulo: ['',Validators.required],
+    firstName: ['',[Validators.required,Validators.minLength(8),Validators.maxLength(50)]],
+    lastName: ['',[Validators.required,Validators.minLength(8),Validators.maxLength(50)]],
+    email: ['',[Validators.required,Validators.email]],
+    telefone: ['',[Validators.required,Validators.minLength(11),Validators.maxLength(12)]],
+    descricao: ['',[Validators.required,Validators.maxLength(30)]],
+    senha: ['',[Validators.required,Validators.minLength(8)]],
+    senhaConfirm: ['',[Validators.required]]
+    }, formOptions);
+  }
+  onSubmit(): void {
+    // Vai parar aqui se o form estiver inválido
+    if (this.form.invalid) {
+      return;
+    }
+  }
+
+  public resetForm(event: any): void {
+    event.preventDefault();
+    this.form.reset();
+  }
 }
--- src/app/components/user/registration/registration.component.ts
+++ src/app/components/user/registration/registration.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AbstractControlOptions, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ValidatorField } from '@app/helpers/ValidatorField';
 
 @Component({
   selector: 'app-registration',
   templateUrl: './registration.component.html',
   styleUrl: './registration.component.scss'
 })
-export class RegistrationComponent {
+export class RegistrationComponent implements OnInit {
+
+
+  form!: FormGroup;
+
+  get f(): any {
+    return this.form.controls;
+  }
+
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.validation();
+  }
+
+  public validation(): void {
+
+    const formOptions: AbstractControlOptions = {
+      validators: ValidatorField.MustMatch('senha','senhaConfirm')
+    };
+
+    this.form = this.fb.group({
+    firstName: ['',[Validators.required,Validators.minLength(8),Validators.maxLength(50)]],
+    lastName: ['',[Validators.required,Validators.minLength(8),Validators.maxLength(50)]],
+    email: ['',[Validators.required,Validators.email]],
+    usuario: ['',[Validators.required,Validators.minLength(8),Validators.maxLength(16)]],
+    senha: ['',[Validators.required,Validators.minLength(8)]],
+    senhaConfirm: ['',[Validators.required]]
+    }, formOptions);
+  }
 
 }
