1ac3a65a9c268fb339011979be7043ff5e36c8dd
marcelo-rivera <marceloriveras@gmail.com>
2025-02-07 16:38:25 -0300

    nav bar stages 07/02/2025

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { AccountService } from '@app/services/account.service';
 
 @Component({
   selector: 'app-navbar',
@@ -10,12 +11,20 @@ export class NavbarComponent implements OnInit {
 
   isCollapsed = true;
 
-  constructor(private router: Router) { }
+  constructor(public accountService: AccountService,
+              @Inject(Router) private router: Router) { }
 
   ngOnInit() {
+    console.log(this.accountService.currentUser$);
+  }
+
+  logout() {
+    this.accountService.logout();
+    this.router.navigateByUrl('/user/login');
   }
 
   showMenu(): boolean {
+    console.log(this.router.url);
     return this.router.url !== 'user/login';
   }
 }
